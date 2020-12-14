8cc1851f9160ec5459a80ca8dc56e84e84e2fc79
maxnocker <maximilian.nocker@konverto.eu>
2020-12-14 09:33:48 +0100

    fix: set resolveJsonModule to true in tsconfig and import .json file

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,15 @@ import Vue from 'vue';
 import App from './App.vue';
 import VueI18n from 'vue-i18n';
 
-import messagesEn from '@/assets/locales/en';
-import messagesDe from '@/assets/locales/de';
-import messagesIt from '@/assets/locales/it';
+import messagesEn from '@/assets/locales/en.json';
+import messagesDe from '@/assets/locales/de.json';
+import messagesIt from '@/assets/locales/it.json';
 
 Vue.config.productionTip = false;
 Vue.use(VueI18n);
 
 const i18n = new VueI18n({
-  locale: 'en',
+  locale: 'de',
   messages: {
     en: messagesEn,
     de: messagesDe,
